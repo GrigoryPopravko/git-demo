2fbdfb21b663f3366f086dedda00a4c460079057
GrigoryPopravko <gregory.popravko@pluto.tv>
2023-03-23 19:02:05 +0100

    name2

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -2,7 +2,6 @@ package org.example.entity;
 
 public class User {
     public static final String name = "Grisha";
-    public static final String name2 = "Grisha";
     public static final String name3 = "Grisha";
     public static final String name4 = "Grisha";
 }
