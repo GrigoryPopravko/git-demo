86c7208c38aefef085c5a7a7e9dbb217f4e0ff32
GrigoryPopravko <gregory.popravko@pluto.tv>
2023-03-23 18:42:26 +0100

    added lastname

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -3,4 +3,5 @@ package org.example.entity;
 public class User {
     public static final String name = "Grisha";
     public static final String surname = "Grisha";
+    public static final String  lastname = "Grisha";
 }
